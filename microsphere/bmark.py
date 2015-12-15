a63f76bc60940f5002fd6bbc7f45a1802836dd72
Joshua A. Anderson <joaander@umich.edu>
2015-12-15 13:58:20 -0500

    Add microsphere benchmark

---
+++ microsphere/bmark.py
@@ -0,0 +1,44 @@
+from __future__ import print_function, division
+from hoomd_script import *
+import numpy
+context.initialize()
+
+# Get the workspace output dir for storing benchmark metadata
+if len(option.get_user()) == 0:
+    workspace = '.';
+else:
+    workspace = option.get_user()[0]
+
+system = init.read_xml(filename='init.xml')
+
+harmonic = bond.harmonic(name="tether")
+harmonic.bond_coeff.set('tether', k=4., r0=0.)
+
+dpd = pair.dpd(r_cut=1.0, T=1.)
+A = 40.0
+dpd.pair_coeff.set(system.particles.types, system.particles.types, A=A, gamma= 1.0)
+dpd.pair_coeff.set('glycerol', 'polymer', A=2*A, gamma= 1.0)
+dpd.pair_coeff.set('hydroxyl', 'polymer', A=2*A, gamma= 1.0)
+dpd.pair_coeff.set('hydroxyl', 'core', A=2*A, gamma= 1.0)
+dpd.pair_coeff.set('hydroxyl', 'glycerol', A=A/2., gamma= 1.0)
+dpd.pair_coeff.set('hydroxyl', 'hydroxyl', A=A/2., gamma= 1.0)
+integrate.mode_standard(dt=0.01)
+integrate.nve(group=group.all())
+
+nlist.reset_exclusions(exclusions = ['bond', 'body'])
+nlist.set_params(r_buff=0.35, check_period=2)
+
+# warm up and autotune
+if globals.exec_conf.isCUDAEnabled():
+    run(10000)
+else:
+    run(10000, limit_hours=20.0/3600.0)
+
+# full benchmark
+tps = benchmark.series(warmup=0, repeat=4, steps=50000, limit_hours=20.0/3600.0)
+ptps = numpy.average(tps) * len(system.particles);
+
+# print out millions of particle time steps per second
+if comm.get_rank() == 0:
+    print("Average particle timesteps per second: {0:.2f} million".format(ptps/1e6));
+    meta.dump_metadata(filename = workspace+"/metadata.json", overwrite = True, user = {'mps': ptps, 'tps': tps});
