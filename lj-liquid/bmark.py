63cc9444484c38eb2c84c582fe518d914e0b6dcd
Joshua A. Anderson <joaander@umich.edu>
2016-07-27 09:52:17 -0400

    Update lj-liquid bmark  for hoomd 2.0

--- lj-liquid/bmark.py
+++ lj-liquid/bmark.py
@@ -1,7 +1,8 @@
 from __future__ import print_function, division
-from hoomd_script import *
+from hoomd import *
+from hoomd import md
 import numpy
-context.initialize()
+c = context.initialize()
 
 # Get the workspace output dir for storing benchmark metadata
 if len(option.get_user()) == 0:
@@ -9,18 +10,19 @@ if len(option.get_user()) == 0:
 else:
     workspace = option.get_user()[0]
 
-system = init.read_xml('init.xml')
-lj = pair.lj(r_cut=3.0)
+system = init.read_gsd('init.gsd')
+nl = md.nlist.cell()
+lj = md.pair.lj(r_cut=3.0, nlist=nl)
 lj.pair_coeff.set('A', 'A', epsilon=1.0, sigma=1.0)
 
 all = group.all()
-integrate.mode_standard(dt=0.005)
-integrate.nvt(group=all, T=1.2, tau=0.5)
+md.integrate.mode_standard(dt=0.005)
+md.integrate.nvt(group=all, kT=1.2, tau=0.5)
 
-nlist.set_params(r_buff=0.6, check_period=7)
+nl.set_params(r_buff=0.6, check_period=7)
 
 # warm up and autotune
-if globals.exec_conf.isCUDAEnabled():
+if c.on_gpu():
     run(30000)
 else:
     run(30000, limit_hours=20.0/3600.0)
@@ -34,4 +36,4 @@ ptps = numpy.average(tps) * len(system.particles);
 # print out millions of particle time steps per second
 if comm.get_rank() == 0:
     print("Hours to complete 10e6 steps: {0}".format(10e6/(ptps/len(system.particles))/3600));
-    meta.dump_metadata(filename = workspace+"/metadata.json", overwrite = True, user = {'mps': ptps, 'tps': tps});
+    meta.dump_metadata(filename = workspace+"/metadata.json", user = {'mps': ptps, 'tps': tps});
