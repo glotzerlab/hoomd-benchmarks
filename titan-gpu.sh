bf6015daed53217f0142969a4452e9716266e294
Joshua A. Anderson <joaander@umich.edu>
2016-09-13 12:29:30 -0400

    Add Titan 64-GPU runs

---
+++ titan-gpu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#PBS -N hoomd-benchmarks
+#PBS -l nodes=64,walltime=2:00:00
+#PBS -A mat110
+#PBS -j oe
+#PBS -m abe
+
+cd ${PBS_O_WORKDIR}
+
+source /ccs/proj/mat110/software/joaander/titan-env1/env.sh
+
+export BENCHMARKS="microsphere hexagon"
+./run-all.sh "flux" "gpu64-$$" "aprun -n 64 -N 1 -b python" "--mode=gpu" "0"
