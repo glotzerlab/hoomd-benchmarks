db78fdac602f9421ad89d6842660ee2133f7e917
Joshua A. Anderson <joaander@umich.edu>
2016-10-24 09:43:51 -0400

    2.1.1 benchmarks on Titan

--- titan-gpu.sh
+++ titan-gpu.sh
@@ -10,4 +10,7 @@ cd ${PBS_O_WORKDIR}
 source /ccs/proj/mat110/software/joaander/titan-env1/env.sh
 
 export BENCHMARKS="microsphere hexagon"
-./run-all.sh "titan" "gpu64-$$" "aprun -n 64 -N 1 -b python" "--mode=gpu" "0"
+./run-all.sh "titan" "titan-1-$$" "aprun -n 1 -N 1 -b python" "--mode=gpu" "0"
+./run-all.sh "titan" "titan-4-$$" "aprun -n 4 -N 1 -b python" "--mode=gpu" "0"
+./run-all.sh "titan" "titan-16-$$" "aprun -n 16 -N 1 -b python" "--mode=gpu" "0"
+./run-all.sh "titan" "titan-64-$$" "aprun -n 64 -N 1 -b python" "--mode=gpu" "0"
