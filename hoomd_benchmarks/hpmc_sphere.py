cffdd696bee0d63116d06d976be3dde33b49f3a1
Joshua A. Anderson <joaander@umich.edu>
2021-08-10 15:49:38 -0400

    Run pre-commit

--- hoomd_benchmarks/hpmc_sphere.py
+++ hoomd_benchmarks/hpmc_sphere.py
@@ -1,3 +1,6 @@
+# Copyright (c) 2021 The Regents of the University of Michigan
+# Part of HOOMD-blue, released under the BSD 3-Clause License.
+
 """Hard sphere Monte Carlo benchmark."""
 
 import hoomd
