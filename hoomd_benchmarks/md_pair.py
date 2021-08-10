1576719f954c030ae78b22d3a41f27f3b00c8352
Joshua A. Anderson <joaander@umich.edu>
2021-08-10 12:38:10 -0400

    Set buffer and rebuild_check_delay from the command line

--- hoomd_benchmarks/md_pair.py
+++ hoomd_benchmarks/md_pair.py
@@ -4,9 +4,20 @@ import hoomd
 from . import common
 from .configuration.hard_sphere import make_hard_sphere_configuration
 
+DEFAULT_BUFFER = 0.4
+DEFAULT_REBUILD_CHECK_DELAY = 1
+
 
 class MDPair(common.Benchmark):
     """Base class pair potential benchmark.
+
+    Args:
+        buffer (float): Neighbor list buffer distance.
+
+        rebuild_check_delay (int): Number of timesteps to run before checking if
+          the neighbor list needs rebuilding.
+
+        kwargs: Keyword arguments accepted by ``Benchmark.__init__``
 
     Derived classes should set the class level variables ``pair_class``,
     ``pair_params``, and ``r_cut``.
@@ -14,6 +25,28 @@ class MDPair(common.Benchmark):
     See Also:
         `common.Benchmark`
     """
+
+    def __init__(self,
+                 buffer=DEFAULT_BUFFER,
+                 rebuild_check_delay=DEFAULT_REBUILD_CHECK_DELAY,
+                 **kwargs):
+        self.buffer = buffer
+        self.rebuild_check_delay = rebuild_check_delay
+        super().__init__(**kwargs)
+
+    @staticmethod
+    def make_argument_parser():
+        """Make an ArgumentParser instance for benchmark options."""
+        parser = common.Benchmark.make_argument_parser()
+        parser.add_argument('--buffer',
+                            type=float,
+                            default=DEFAULT_BUFFER,
+                            help='Neighbor list buffer.')
+        parser.add_argument('--rebuild_check_delay',
+                            type=int,
+                            default=DEFAULT_REBUILD_CHECK_DELAY,
+                            help='Neighbor list rebuild check delay.')
+        return parser
 
     def make_simulation(self):
         """Make the Simulation object."""
@@ -25,6 +58,9 @@ class MDPair(common.Benchmark):
 
         integrator = hoomd.md.Integrator(dt=0.005)
         cell = hoomd.md.nlist.Cell()
+        cell.rebuild_check_delay = self.rebuild_check_delay
+        cell.buffer = self.buffer
+
         pair = self.pair_class(nlist=cell)
         pair.params[('A', 'A')] = self.pair_params
         pair.r_cut[('A', 'A')] = self.r_cut
