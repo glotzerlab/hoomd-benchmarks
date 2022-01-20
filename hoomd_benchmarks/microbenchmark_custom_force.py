5796b488dac431fda654e606164a782fa9f1ee34
Tommy Waltmann <tomwalt@umich.edu>
2022-01-20 13:47:38 -0500

    add custom force benchmark

---
+++ hoomd_benchmarks/microbenchmark_custom_force.py
@@ -0,0 +1,55 @@
+import hoomd
+from . import common
+from .configuration.hard_sphere import make_hard_sphere_configuration
+
+
+class EmptyForce(hoomd.md.force.Custom):
+    """Custom force which calls an empty set_forces method."""
+
+    def __init__(self):
+        super().__init__()
+
+    def set_forces(self, timestep):
+        pass
+
+
+class MicrobenchmarkCustomForce(common.ComparativeBenchmark):
+    """Measure the overhead of using a custom force.
+
+    This benchmark performs an
+    """
+
+    def make_simulations(self):
+        """Make the simulation objects."""
+        path = make_hard_sphere_configuration(N=self.N,
+                                              rho=self.rho,
+                                              dimensions=self.dimensions,
+                                              device=self.device,
+                                              verbose=self.verbose)
+
+        sim0 = hoomd.Simulation(device=self.device, seed=100)
+        sim0.create_state_from_gsd(filename=str(path))
+        sim0.operations.updaters.clear()
+        sim0.operations.computes.clear()
+        sim0.operations.writers.clear()
+        sim0.operations.tuners.clear()
+        sim0.operations.integrator = hoomd.md.Integrator(
+            dt=0.001, methods=[hoomd.md.methods.NVE(filter=hoomd.filter.All())])
+
+        sim1 = hoomd.Simulation(device=self.device, seed=100)
+        sim1.create_state_from_gsd(filename=str(path))
+        sim1.operations.updaters.clear()
+        sim1.operations.computes.clear()
+        sim1.operations.writers.clear()
+        sim1.operations.tuners.clear()
+        sim1.operations.integrator = hoomd.md.Integrator(
+            dt=0.001, methods=[hoomd.md.methods.NVE(filter=hoomd.filter.All())])
+
+        empty_custom_force = EmptyCustomForce()
+        sim1.operations.integrator.forces.append(empty_custom_force)
+
+        return sim0, sim1
+
+
+if __name__ == '__main__':
+    MicrobenchmarkCustomForce.main()
