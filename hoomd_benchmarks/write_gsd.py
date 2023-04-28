cf4a030c6c1dd17b10fbc306125abe6b6820369f
Joshua A. Anderson <joaander@umich.edu>
2023-04-28 09:09:58 -0400

    Initial working version of gsd benchmarks.

---
+++ hoomd_benchmarks/write_gsd.py
@@ -0,0 +1,104 @@
+# Copyright (c) 2021-2023 The Regents of the University of Michigan
+# Part of HOOMD-blue, released under the BSD 3-Clause License.
+
+"""Benchmark GSD writes."""
+
+import hoomd
+import time
+from . import common
+from .configuration.hard_sphere import make_hard_sphere_configuration
+
+DEFAULT_BANDWIDTH = 'false'
+DEFAULT_MAXIMUM_WRITE_BUFFER_SIZE = 64 * 1024 * 1024
+
+
+class GSD(common.Benchmark):
+    """Base class GSD benchmark.
+
+    Args:
+        kwargs: Keyword arguments accepted by ``Benchmark.__init__``
+
+    Derived classes should set override `make_write_gsd` to implement
+    different options.
+
+    See Also:
+        `common.Benchmark`
+    """
+    SUITE_STEP_SCALE = 0.01
+
+    def __init__(self,
+                 bandwidth=DEFAULT_BANDWIDTH,
+                 maximum_write_buffer_size=DEFAULT_MAXIMUM_WRITE_BUFFER_SIZE,
+                 **kwargs):
+        self.bandwidth = bandwidth
+        self.maximum_write_buffer_size = maximum_write_buffer_size
+
+        if bandwidth:
+            self.units = 'MiB/s'
+
+        super().__init__(**kwargs)
+
+    @staticmethod
+    def make_argument_parser():
+        """Make an ArgumentParser instance for benchmark options."""
+        parser = common.Benchmark.make_argument_parser()
+        parser.add_argument('--bandwidth',
+                            action='store_true',
+                            help='Report performance in bandwidth.')
+        parser.add_argument('--maximum_write_buffer_size',
+                            type=int,
+                            default=DEFAULT_MAXIMUM_WRITE_BUFFER_SIZE,
+                            help='Maximum size of the write buffer (in bytes).')
+        return parser
+
+    def make_write_gsd(self):
+        """Make the GSD writer object for benchmarking."""
+        writer = hoomd.write.GSD(trigger=hoomd.trigger.Periodic(1),
+                               filename='write_gsd.gsd',
+                               mode='wb')
+
+        try:
+            writer.dynamic=['particles/position']
+        except hoomd.error.TypeConversionError:
+            pass
+
+        return writer
+
+    def make_simulation(self):
+        """Make the Simulation object."""
+        path = make_hard_sphere_configuration(N=self.N,
+                                              rho=self.rho,
+                                              dimensions=self.dimensions,
+                                              device=self.device,
+                                              verbose=self.verbose)
+
+        sim = hoomd.Simulation(device=self.device)
+        sim.create_state_from_gsd(filename=str(path))
+
+        self.writer = self.make_write_gsd()
+        self.writer.maximum_write_buffer_size = self.maximum_write_buffer_size
+        sim.operations.writers.append(self.writer)
+
+        return sim
+
+    def run(self, steps):
+        """Run the benchmark for the given number of steps."""
+        start_time = time.time()
+
+        self.sim.run(steps)
+        if hasattr(self.writer, 'flush'):
+            self.writer.flush()
+        end_time = time.time()
+
+        self.tps = steps / (end_time - start_time)
+
+    def get_performance(self):
+        """Get the performance of the benchmark during the last ``run``."""
+        if self.bandwidth:
+            return self.N * 4 * 3 / 1024**2 * self.tps
+        else:
+            return self.tps
+
+
+if __name__ == '__main__':
+    GSD.main()
---
+++ hoomd_benchmarks/write_gsd_log.py
@@ -0,0 +1,36 @@
+# Copyright (c) 2021-2023 The Regents of the University of Michigan
+# Part of HOOMD-blue, released under the BSD 3-Clause License.
+
+"""Benchmark GSD writes."""
+
+import hoomd
+from . import write_gsd
+
+
+class GSDLog(write_gsd.GSD):
+    """Log-only GSD benchmark.
+
+    Args:
+        kwargs: Keyword arguments accepted by ``GSD.__init__``
+
+    See Also:
+        `write_gsd.GSD`
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def make_write_gsd(self):
+        """Make the GSD writer object for benchmarking."""
+        logger = hoomd.logging.Logger(categories=['scalar', 'string'])
+        logger[('value')] = (lambda: 42, 'scalar')
+
+        return hoomd.write.GSD(trigger=hoomd.trigger.Periodic(1),
+                               filename='write_gsd_log.gsd',
+                               mode='wb',
+                               filter=hoomd.filter.Null(),
+                               logger=logger)
+
+
+if __name__ == '__main__':
+    GSDLog.main()
