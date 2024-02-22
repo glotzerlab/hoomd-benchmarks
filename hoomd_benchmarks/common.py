bc64acc403e62b807fa8b709918cb89aa945c475
Joshua A. Anderson <joaander@umich.edu>
2024-02-22 09:39:24 -0500

    Update copyright year.

--- hoomd_benchmarks/common.py
+++ hoomd_benchmarks/common.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2021-2023 The Regents of the University of Michigan
+# Copyright (c) 2021-2024 The Regents of the University of Michigan
 # Part of HOOMD-blue, released under the BSD 3-Clause License.
 
 """Common code used in all benchmarks."""
