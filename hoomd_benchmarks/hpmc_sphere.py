37988aa9429ecc37fe2610d6146f74994b1434ff
Joshua A. Anderson <joaander@umich.edu>
2026-03-11 12:29:14 -0400

    Research relevant sphere benchmark.

--- hoomd_benchmarks/hpmc_sphere.py
+++ hoomd_benchmarks/hpmc_sphere.py
@@ -3,10 +3,12 @@
 
 """Hard sphere Monte Carlo benchmark."""
 
+import math
+
 import hoomd
 
 from . import hpmc_base
-from .configuration.hard_sphere import make_hard_sphere_configuration
+from .configuration.hard_shape import make_hard_shape_configuration
 
 
 class HPMCSphere(hpmc_base.HPMCBenchmark):
@@ -18,34 +20,36 @@ class HPMCSphere(hpmc_base.HPMCBenchmark):
 
     def make_simulation(self):
         """Make the Simulation object."""
-        path = make_hard_sphere_configuration(
+        mc = hoomd.hpmc.integrate.Sphere()
+        mc.shape['A'] = dict(diameter=1.0)
+
+        r = 0.5
+        if self.dimensions == 2:
+            sphere_volume = math.pi * r**2
+            default_d = 0.6887305181920008
+        if self.dimensions == 3:
+            sphere_volume = 4.0 / 3.0 * math.pi * r**3
+            default_d = 0.11295954044540771
+        
+        path = make_hard_shape_configuration(
+            name="sphere",
             N=self.N,
-            rho=1.0,
+            integrator=mc,
+            phi=0.50,
+            particle_volume=sphere_volume,
             dimensions=self.dimensions,
             device=self.device,
             verbose=self.verbose,
         )
 
-        mc = hoomd.hpmc.integrate.Sphere()
+        mc = hoomd.hpmc.integrate.Sphere(default_d=default_d)
         mc.shape['A'] = dict(diameter=1.0)
 
         sim = hoomd.Simulation(device=self.device, seed=100)
         sim.create_state_from_gsd(filename=str(path))
         sim.operations.integrator = mc
 
-        self.units = 'trial moves per second per particle'
-
         return sim
-
-    def get_performance(self):
-        """Get the benchmark performance."""
-        mc = self.sim.operations.integrator
-        return (
-            (sum(mc.translate_moves) + sum(mc.rotate_moves))
-            / self.sim.walltime
-            / self.sim.state.N_particles
-        )
-
 
 if __name__ == '__main__':
     HPMCSphere.main()
